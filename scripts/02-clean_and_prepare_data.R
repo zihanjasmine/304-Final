b202f91e06ddc04fcffd6f2c4048d7276a1d741f
zihanjasmine <zhanzihan.zhang@mail.utoronto.ca>
2022-04-26 11:19:18 -0400

    clean data

--- scripts/02-clean_and_prepare_data.R
+++ scripts/02-clean_and_prepare_data.R
@@ -1,15 +1,33 @@
+
+
 #### Preamble ####
-# Purpose: Clean the data downloaded from Ontario Data Catalogue.
-# Author: Zihan Zhang
-# Data: 28 March 2022
-# Contact: zhanzihan.zhang@mail.utoronto.ca 
-# License: MIT
+# Purpose: Clean the data downloaded from Open Data Toronto.
+# Author: Zihan Zhang  
+# Email: zhanzihan.zhang@mail.utoronto.ca
+# Date:25 May 2022
+ 
 
-
-
-
-#### Workspace setup ####
-
-# Load all the packages
+#### Workspace set-up ####
+library(janitor)
+library(lubridate)
 library(tidyverse)
 
+#### Clean data ####
+
+
+raw <- read_csv("inputs/data/raw.csv")
+
+
+# clean variable names 
+cleaned_data <- raw %>%
+  clean_names()
+
+cleaned_data <- cleaned_data %>%
+# remove na values in age group
+  filter(age_group != "NA") %>%
+  select(-assigned_id) %>%
+  mutate(hosptial= case_when(ever_hospitalized == "Yes" ~ "1",
+                         TRUE ~"0"))
+  
+# export cleaned data
+write_csv(cleaned_data, "inputs/data/cleaned_data.csv")
